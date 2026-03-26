94067ae88f7efc86503e37d46a9ecdf4dcbb4d99
odo1212 <93018886+odo1212@users.noreply.github.com>
2026-03-26 15:45:04 +0900

    [Bronze II] Title: 분산처리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    result = pow(a, b, 10)
+    if result == 0:
+        print(10)
+    else:
+        print(result)
